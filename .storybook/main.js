5f3fe899d1454606f446e9ef9c8463abbe0203b3
Cory Redmond <git@cory.red>
2022-07-06 22:00:28 +0100

    aaaa

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,57 @@
-const path = require("path");
-
+// const path = require("path");
+//
+// module.exports = {
+//   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+//   addons: [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-interactions",
+//     "@storybook/preset-create-react-app",
+//     {
+//       name: "@storybook/preset-scss",
+//       options: {
+//         cssLoaderOptions: {
+//           modules: { localIdentName: "[name]__[local]--[hash:base64:5]" },
+//         },
+//       },
+//     },
+//   ],
+//   framework: "@storybook/react",
+//   core: {
+//     disableTelemetry: true,
+//     builder: "@storybook/builder-webpack5",
+//   },
+// };
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/core/components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
-    {
-      name: "@storybook/preset-scss",
-      options: {
-        cssLoaderOptions: {
-          modules: { localIdentName: "[name]__[local]--[hash:base64:5]" },
-        },
-      },
-    },
+    "@storybook/preset-ant-design",
   ],
   framework: "@storybook/react",
   core: {
-    disableTelemetry: true,
     builder: "@storybook/builder-webpack5",
+    webpackFinal: async (config, { configType }) => {
+      // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+      // You can change the configuration based on that.
+      // 'PRODUCTION' is used when building the static version of storybook.
+
+      // Make whatever fine-grained changes you need
+      config.module.rules.push({
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "../"),
+      });
+
+      // Return the altered config
+      return config;
+    },
   },
 };
